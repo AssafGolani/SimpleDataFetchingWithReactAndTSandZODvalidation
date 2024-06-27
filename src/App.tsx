0c645d4c18b295c36c6240323324383aa8d42c9b
Assaf <1214assaf@gmail.com>
2024-06-27 13:21:32 +0300

    chore: Implement data fetching functionality in App component

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,48 @@
+import { type ReactNode, useEffect, useState } from "react";
+import { get } from "./util/http";
+import BlogPosts, { BlogPost } from "./components/BlogPosts";
+import fetchingImg from "./assets/data-fetching.png";
+type RawDataBlogPost = {
+  id: number;
+  userId: number;
+  title: string;
+  body: string;
+};
+
 function App() {
-  return <h1>Data Fetching!</h1>;
+  const [fetchedPosts, setFetchedPosts] = useState<BlogPost[]>();
+
+  useEffect(() => {
+    async function fetchData() {
+      const data: RawDataBlogPost[] = (await get(
+        "https://jsonplaceholder.typicode.com/posts"
+      )) as RawDataBlogPost[];
+
+      const blogPosts: BlogPost[] = data.map((rawPost) => {
+        return {
+          id: rawPost.id,
+          title: rawPost.title,
+          text: rawPost.body,
+        };
+      });
+
+      setFetchedPosts(blogPosts);
+    }
+
+    fetchData();
+  }, []);
+
+  let content: ReactNode;
+  if (fetchedPosts) {
+    content = <BlogPosts posts={fetchedPosts} />;
+  }
+
+  return (
+    <main>
+      <img src={fetchingImg} alt="an abstract image" />
+      {content}
+    </main>
+  );
 }
 
 export default App;
